6087ddfbd27f7a6d01798b07b92debb36631272a
Claudia S Cabrera <miradacsc@gmail.com>
2022-09-24 16:54:31 -0300

    Añado boostrap y un form a agregar usuario

--- cliente/src/AgregarUsuario.js
+++ cliente/src/AgregarUsuario.js
@@ -2,9 +2,36 @@ import React from 'react'
 
 //nuevo componente
 function AgregarUsuario(){
+    function agregarUsuario(){
+        
+    }
+
     return(
-        <div>
-            <h2>Crear un nuevo usuario</h2>
+        <div className='container'>
+            <div className='row'>
+            <h2 className='mt-4'>Crear un nuevo usuario</h2>
+            </div>
+
+            <div className='row'>
+                <div className='col-sm-6 offset-3'>
+                <div className='mb-3'>
+                    <label htmlFor="nombre" className='form-1label'>Nombre</label>
+                    <input type="text" className="form-control"></input>
+                </div>
+
+                <div className='mb-3'>
+                    <label htmlFor="email" className='form-1label'>Email</label>
+                    <input type="email" className="form-control"></input>
+                </div>
+
+                <div className='mb-3'>
+                    <label htmlFor="telefono" className='form-1label'>Teléfono</label>
+                    <input type="text" className="form-control"></input>
+                </div>
+
+                <button onClick={agregarUsuario} className="btn btn-success">Guardar Usuario</button>
+                </div>
+            </div>
         </div>
     )
 }
